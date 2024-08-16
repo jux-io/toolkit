1910886e814f40a4a7254841d5529b1950af5903
tnipri <tom@jux.io>
2024-08-17 01:56:46 +0300

    refactor(@juxio/react-styled): modified lib to support types overriding with declare module

--- packages/react/styled/src/styled/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './base';
-export { default as styled } from './styled';
-export { default as css } from './css';
--- packages/react/styled/tsup.config.ts
+++ packages/react/styled/tsup.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  name: '@juxio/react-styled',
-  entry: ['./src/**/*'],
-  tsconfig: './tsconfig.json',
-  format: ['cjs', 'esm'],
-  splitting: false,
-  dts: true,
-  shims: true,
-  clean: true,
-});
+export default defineConfig([
+  {
+    name: '@juxio/react-styled',
+    entry: ['./src/index.ts', './src/tokens.ts'],
+    tsconfig: './tsconfig.json',
+    format: ['cjs', 'esm'],
+    splitting: true,
+    sourcemap: true,
+    cjsInterop: true,
+    treeshake: true,
+    dts: true,
+    clean: true,
+    external: ['react'],
+  },
+]);
