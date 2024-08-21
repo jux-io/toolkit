a0c8a01f208dbe4d38155bf02a1da874464d328e
tnipri <tom@jux.io>
2024-08-21 23:18:30 +0300

    fix(@juxio/cli): fixed an issue with pkg mngr detection on cli

---
+++ .changeset/old-lemons-unite.md
@@ -0,0 +1,7 @@
+---
+'@juxio/core': patch
+'@juxio/cli': patch
+---
+
+- added version command to cli
+- fixed an issue where the cli installs invalid packages on init
--- packages/core/src/utils/get-package-manager.ts
+++ packages/core/src/utils/get-package-manager.ts
@@ -5,8 +5,9 @@ export async function getPackageManager(
 ): Promise<'yarn' | 'pnpm' | 'bun' | 'npm'> {
   const packageManager = await detect({ programmatic: true, cwd: targetDir });
 
-  if (packageManager === 'yarn@berry') return 'yarn';
-  if (packageManager === 'pnpm@6') return 'pnpm';
+  if (packageManager === 'yarn@berry' || packageManager === 'yarn')
+    return 'yarn';
+  if (packageManager === 'pnpm@6' || packageManager === 'pnpm') return 'pnpm';
   if (packageManager === 'bun') return 'bun';
 
   return packageManager ?? 'npm';
