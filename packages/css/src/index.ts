aefefa79b7d8a4cf4be43e5eb84dd386e9bfe497
tnipri <tom@jux.io>
2024-09-13 22:56:06 +0300

    fix(@juxio/react-styled): fixed an issue with types in react-styled and css libraries

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -1,3 +1,25 @@
+import {
+  CSSProperties,
+  CSSPropertiesWithCustomValues,
+  CustomTokensValues,
+  Merge,
+} from './base';
+import cssFunction from './css';
+import { Conditions, Tokens, Utilities } from './types';
+
+export type CSS = (
+  styles: CSSPropertiesWithCustomValues<
+    Merge<CSSProperties, CustomTokensValues<Tokens>>,
+    Utilities,
+    Conditions
+  >
+) => string;
+
+/**
+ * CSS function that returns a string of class name.
+ */
+const css: CSS = cssFunction;
+
 export * from './base';
-export { css } from './css';
+export { css };
 export { calculateHash } from './utils/calculate-hash';
