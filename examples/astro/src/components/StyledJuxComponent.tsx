cdccb3a3965d51879b06392a521270b7f29cc94c
Yoav Ganabr <yoav.ganbar@gmail.com>
2024-09-25 23:51:33 +0300

    chore: astro init fail

---
+++ examples/astro/src/components/StyledJuxComponent.tsx
@@ -0,0 +1,32 @@
+import { css } from '@juxio/css';
+
+export const titleStyle = css({
+  color: 'violet',
+  '&:hover': {
+    color: 'darkviolet',
+  },
+});
+
+export const MyStyledComponent = ({
+  children,
+  title,
+}: React.PropsWithChildren & { title: string }) => {
+  return (
+    <div
+      className={css({
+        border: '1px solid {color.core.grays_500}',
+        borderRadius: '{core.dimension.spacing_100}',
+        padding: '1.5rem',
+        flexDirection: 'column',
+        gap: 'clamp(0.5rem, calc(0.125rem + 1vw), 1rem)',
+
+        [`.${titleStyle}`]: {
+          marginBottom: '1rem',
+        },
+      })}
+    >
+      <span className={titleStyle}>{title}</span>
+      {children}
+    </div>
+  );
+};
