60ca7e8e84f7d435f36f835572231e0c69b84510
Yoav Ganabr <yoav.ganbar@gmail.com>
2024-09-26 00:01:49 +0300

    docs: css function example in vite

--- examples/vite-react-app/src/App.tsx
+++ examples/vite-react-app/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { css } from '@juxio/css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +18,17 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div
+        className={css({
+          color: 'violet',
+          fontSize: '2rem',
+          '&:hover': {
+            color: 'darkviolet',
+          },
+        })}
+      >
+        Hello from Jux in Vite! 🚀
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +41,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
