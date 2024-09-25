7b3046dee60a425d4c19d44e3271819d18046aba
Yoav Ganabr <yoav.ganbar@gmail.com>
2024-09-25 22:01:34 +0300

    chore: add pulled editor component clean up next boilerplate styles

--- examples/nextjs-app/src/app/page.tsx
+++ examples/nextjs-app/src/app/page.tsx
@@ -1,11 +1,20 @@
 import styles from './page.module.css';
 import { MyStyledComponent } from './components/my-component';
+import { MyButton } from '@/jux/components/MyButton';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <MyStyledComponent title="Jux component" size="small" />
-      <MyStyledComponent title="Jux component 2" size="medium" />
+      <MyStyledComponent title="Jux component" size="small">
+        I am small!
+      </MyStyledComponent>
+      <MyStyledComponent title="Jux component 2" size="medium">
+        Hello from component 2! I am medium.
+      </MyStyledComponent>
+      <section>
+        <h2>Check out my imported button:</h2>
+        <MyButton hierarchy="primary" disabled={false} />
+      </section>
     </div>
   );
 }
