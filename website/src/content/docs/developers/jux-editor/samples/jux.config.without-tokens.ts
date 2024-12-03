249d7889bb189c27bc0defb82d8f98adfe2d6df4
Yoav Ganabr <yoav.ganbar@gmail.com>
2024-12-03 16:31:33 +0200

    docs: update

--- website/src/content/docs/developers/jux-editor/samples/jux.config.without-tokens.ts
+++ website/src/content/docs/developers/jux-editor/samples/jux.config.without-tokens.ts
@@ -3,24 +3,7 @@ import { defineConfig } from '@juxio/cli';
 export default defineConfig({
   /* ... */
   tokens_directory: './src/jux/tokens',
-
   components_directory: './src/jux/components',
-
   definitions_directory: './src/jux/types',
-
-  core_tokens: {
-    /* Your core tokens */
-  },
-
-  themes: {
-    light: {
-      /* Your light theme */
-    },
-    dark: {
-      /* Your dark theme */
-    },
-    my_custom_theme: {
-      /* Your custom theme */
-    },
-  },
+  /* ... */
 });
