7c3afc366f62db9b931c5149038005959eccab2d
Yoav Ganabr <yoav.ganbar@gmail.com>
2024-12-26 16:44:27 +0200

    refactor(@juxio/react-primitives): update select export to named export for better modularity

--- packages/primitives/react/src/primitives/index.ts
+++ packages/primitives/react/src/primitives/index.ts
@@ -1,8 +1,7 @@
 export * from './checkbox/Checkbox';
 export * from './label/Label';
 export * from './generic-slot/GenericSlot';
-export * from './select';
+export * as Select from './select';
 export * from './base/BasePrimitives';
 export * from './toggle/Toggle';
 export * from './radio/Radio';
-export * from './select';
